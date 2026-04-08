97f5f37604cb00e76f495c74adeeae25c7e69fd6
shPallavi <singhhhpallavi@gmail.com>
2026-04-08 21:14:18 +0530

    Flatten Array Problem using Built-in method or Recusrsion

--- Day-10.js
+++ Day-10.js
@@ -1,27 +1,30 @@
-// Party of Couples
-// Difficulty: EasyAccuracy: 50.5%Submissions: 86K+Points: 2
-// You are given an integer array arr[] of size n, representing n number of people
-// in a party, each person is denoted by an integer. Couples are represented by
-// the same number ie: two people have the same integer value, it means they are a couple.
-// Find out the only single person in the party of couples.
-// Note: It is guarantee that there exist only one single person in the party.
+// 🔹 Problem Statement
+// Write a function flatten(arr) that takes a nested array
+// and returns a new array flattened into a single level.
 
-// Input: arr[] = [1, 2, 3, 2, 1]
-// Output: 3
-// Explaination: Only the number 3 is single.
-// Input: arr[] = [1, 2, 3, 5, 3, 2, 1, 4, 5, 6, 6]
-// Output: 4
-// Explaination: 4 is the only single.
-// Input: arr[] = [1, 1, 3, 3, 2]
-// Output: 2
+// 🔹 Built-in Method
+const input = [1, [2, 3], [4, [5, 6]]];
+console.log(input.flat(Infinity));
+// Output: [1, 2, 3, 4, 5, 6]
 
-function findSinglePerson(arr) {
-  let single = 0;
-  for (let num of arr) {
-    single ^= num;
+// 🔹 Recursion
+function flatten(arr) {
+  let result = [];
+  for (let item of arr) {
+    if (Array.isArray(item)) {
+      result = result.concat(flatten(item));
+    } else {
+      result.push(item);
+    }
   }
-  return single;
+  return result;
 }
-console.log(findSinglePerson([1, 2, 3, 2, 1]));
-console.log(findSinglePerson([1, 2, 3, 5, 3, 2, 1, 4, 5, 6, 6]));
-console.log(findSinglePerson([1, 1, 3, 3, 2]));
+console.log(flatten([1, [2, 3], [4, [5, 6]]]));
+console.log(flatten([1, [2, 3]]));
+console.log(
+  flatten([
+    [1, 2],
+    [3, 4],
+  ]),
+);
+console.log(flatten([1, 2, 3]));
