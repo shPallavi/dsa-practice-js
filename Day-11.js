95f843e80f13d258f203fd6dd8431e530d0435ed
shPallavi <singhhhpallavi@gmail.com>
2026-04-09 21:26:26 +0530

    Day-10: sortedFrequency counts occurrences in sorted array

---
+++ Day-11.js
@@ -0,0 +1,21 @@
+// Divide and Conquer - sortedFrequency
+// Given a sorted array and a number, write a function called
+// sortedFrequency that counts the occurrences of the number in the array
+
+// sortedFrequency([1,1,2,2,2,2,3],2) // 4
+// sortedFrequency([1,1,2,2,2,2,3],3) // 1
+// sortedFrequency([1,1,2,2,2,2,3],1) // 2
+// sortedFrequency([1,1,2,2,2,2,3],4) // -1
+// Time Complexity - O(n)
+
+function sortedFrequency(arr, num) {
+  let count = 0;
+  for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] === num) count++;
+  }
+  return count === 0 ? -1 : count;
+}
+console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 2));
+console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 3));
+console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 1));
+console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 4));
