1d897a6797d30dec04dbed555c96ed821e91fa4f
shPallavi <singhhhpallavi@gmail.com>
2026-04-11 22:36:38 +0530

    Day-13 isPalindrome or not

---
+++ Day-13.js
@@ -0,0 +1,21 @@
+// isPalindrome
+// Write a recursive function called isPalindrome which
+// returns true if the string passed to it is a palindrome(reads the same forward and backward).
+// Otherwise it returns false.
+
+// isPalindrome('awesome') // false
+// isPalindrome('foobar') // false
+// isPalindrome('tacocat') // true
+// isPalindrome('amanaplanacanalpanama') // true
+// isPalindrome('amanaplanacanalpandemonium') // false
+
+function isPalindrome(str, left = 0, right = str.length - 1) {
+  if (left >= right) return true;
+  if (str[left] !== str[right]) return false;
+  return isPalindrome(str, left + 1, right - 1);
+}
+console.log(isPalindrome("awesome"));
+console.log(isPalindrome("foobar"));
+console.log(isPalindrome("tacocat"));
+console.log(isPalindrome("amanaplanacanalpanama"));
+console.log(isPalindrome("amanaplanacanalpandemonium"));
